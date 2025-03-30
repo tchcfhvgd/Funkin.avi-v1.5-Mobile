99df43f6d7f7bd2d259e1de389d883cd8690ae01
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-03-30 10:22:51 +0800

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,5 +1,6 @@
 package;
 
+#if desktop
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
 
@@ -107,3 +108,4 @@ class DiscordClient
 	} //werid about you can do something of this in lua
 	#end
 }
+#end
